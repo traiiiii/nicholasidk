70bda704759c85aa289d78adea0dc85839efa17c
Trai Leammookda <traitaw@gmail.com>
2025-11-11 21:58:33 +0700

    changed middleware to proxy

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ import { decrypt } from "./app/lib/session";
 const protectedRoutes = ["/stress", "/"]
 const publicRoutes = ["/login"]
 
-export default async function middleware(req) {
+export default async function proxy(req) {
     const path = req.nextUrl.pathname
     const isProtectedRoute = protectedRoutes.includes(path)
     const isPublicRoute = publicRoutes.includes(path)
